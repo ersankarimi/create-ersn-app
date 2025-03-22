d1e65b4b2b9bdbb09c1720fe0eb6b4933192ec92
Ersan Karimi <ersankarimii311@gmail.com>
2025-03-22 21:03:12 +0800

    feat: enhance template selection and project name handling with new utility functions

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
 
 import type { TemplateKey } from "templates";
-import { intro, log, outro, select, text } from "@clack/prompts";
+import path from "node:path";
+import process from "node:process";
+import { intro, log, outro, select, spinner, text } from "@clack/prompts";
 import chalk from "chalk";
 import { COLORS } from "colors";
 import filenamify from "filenamify";
 import gradient from "gradient-string";
 import { getTemplateByKey, TEMPLATES } from "templates";
-import { handleCancel, isStringContainSpaces, shouldContinue } from "utils";
+import { cloneGithubRepo, getPackageManager, handleCancel, handleDestinationNoEmpty, isStringContainSpaces, shouldContinue } from "utils";
 import isValidFilename from "valid-filename";
 
 const state: {
@@ -17,6 +19,8 @@ const state: {
   selectedTemplateKey: null,
   projectName: "my-app",
 };
+
+const currentWorkingDir = process.cwd();
 
 const banner = gradient([
   {
@@ -43,7 +47,7 @@ const banner = gradient([
 
 async function selectingTemplateKey() {
   try {
-    const templateKey = await select({
+    const userTemplateKey = await select({
       message: "Choose what starter template you want to use",
       options: TEMPLATES.map((item) => {
         return {
@@ -54,14 +58,14 @@ async function selectingTemplateKey() {
       }),
     });
 
-    await handleCancel(templateKey);
+    await handleCancel(userTemplateKey);
 
-    if (typeof templateKey === "symbol") {
+    if (typeof userTemplateKey === "symbol") {
       throw new TypeError("You didn't select a Template");
     }
     else {
-      state.selectedTemplateKey = templateKey;
-      const selectedTemplate = getTemplateByKey(templateKey);
+      state.selectedTemplateKey = userTemplateKey;
+      const selectedTemplate = getTemplateByKey(userTemplateKey);
       log.info(
         `Selected starter template: ${selectedTemplate?.title}`,
       );
@@ -77,54 +81,123 @@ async function selectingTemplateKey() {
 
 async function setProjectName() {
   try {
-    const projectName = await text({
+    const userProjectName = await text({
       message: "Project name:",
       initialValue: "",
       defaultValue: "",
       placeholder: state.projectName,
-    }).then((value) => {
-      return String(value);
     });
 
-    if (!projectName) {
-      log.info(`Project name is set to default: ${state.projectName}`);
+    await handleCancel(userProjectName);
+
+    if (typeof userProjectName === "symbol") {
+      throw new TypeError("You didn't set a project name");
     }
 
-    if (!isValidFilename(projectName) || isStringContainSpaces(projectName)) {
-      const formattedProjectName = isStringContainSpaces(projectName) ? projectName.split(" ").join("-") : filenamify(projectName, { replacement: "-" });
+    if (!userProjectName) {
+      log.info(`Project name is set to default: ${state.projectName}`);
+    }
+    else if (userProjectName !== "." && (!isValidFilename(userProjectName) || isStringContainSpaces(userProjectName))) {
+      const formattedProjectName = isStringContainSpaces(userProjectName) ? userProjectName.split(" ").join("-") : filenamify(userProjectName, { replacement: "-" });
 
       log.info(`Project name is set to: ${formattedProjectName}`);
       state.projectName = formattedProjectName;
+    }
+    else if (userProjectName === ".") {
+      const projectName = currentWorkingDir.split("/").pop() || "";
+      state.projectName = projectName || state.projectName;
+      log.info(`Project name is set to current working dir: ${projectName}`);
+    }
+    else {
+      state.projectName = userProjectName;
+      log.info(`Project name: ${userProjectName}`);
     }
   }
   catch (error) {
     if (error instanceof Error) {
       log.error(error.message);
+      await shouldContinue(setProjectName);
+    }
+  }
+}
+
+async function downloadTheTemplate() {
+  const s = spinner();
+
+  if (!state.selectedTemplateKey) {
+    throw new TypeError("There is no selected template key!");
+  }
+
+  try {
+    const fullDestination = path.join(
+      process.cwd(),
+      state.projectName,
+    );
+
+    await handleDestinationNoEmpty(fullDestination);
+
+    s.start("Downloading the template ....");
+
+    await cloneGithubRepo(state.selectedTemplateKey, fullDestination);
+
+    s.stop(
+      "✅ Successfully downloaded the template",
+    );
+  }
+  catch (error) {
+    if (error instanceof Error) {
+      s.stop(
+        "❌ Failed to download template",
+      );
+      log.error(error.message);
+      outro(
+        "Failed to download the template. Please try again.",
+      );
     }
   }
 }
 
 async function main() {
-  console.clear();
-  console.log(banner);
-  console.log();
+  try {
+    console.clear();
+    console.log(banner);
+    console.log();
 
-  intro(
-    chalk.bold(
-      gradient([COLORS.purple, COLORS.pink])("Welcome to create-ersn-app!"),
-    ),
-  );
+    intro(
+      chalk.bold(
+        gradient([COLORS.purple, COLORS.pink])("Welcome to create-ersn-app!"),
+      ),
+    );
 
-  await selectingTemplateKey();
-  await setProjectName();
+    await selectingTemplateKey();
+    await setProjectName();
 
-  outro(`You're all set!`);
+    if (state.projectName && state.selectedTemplateKey) {
+      await downloadTheTemplate();
+    }
+  }
+  catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+  }
+  finally {
+    outro("Done, now run:");
+
+    console.log(`
+  ${chalk.bold("cd")} ${state.projectName}
+  ${chalk.bold(`${getPackageManager()} install`)}`);
+
+    process.exit(0);
+  }
 }
 
 main().catch((error) => {
-  console.log("annjing", error);
   if (error instanceof Error) {
-    console.log("anjingg");
     console.error(error.message);
+    log.error(error.message);
+  }
+  else {
+    log.error("Something went wrong!");
   }
 });
